93c69f8f54288b6cc3f048a1ce11d0ef578c1983
sania-owner <saniaowner@ya.ru>
2022-04-03 20:57:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,8 +25,10 @@ python3 -m pip install tabulate
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git /root/mhddos_proxy
 wget -O /root/start_layer4.sh https://raw.githubusercontent.com/sania-owner/DDoS/main/layer4.sh
 wget -O /root/start_layer7.sh https://raw.githubusercontent.com/sania-owner/DDoS/main/layer7.sh
+wget -O /root/start_layer7.sh https://raw.githubusercontent.com/sania-owner/DDoS/main/start.sh
 chmod 0777 /root/layer4.sh
 chmod 0777 /root/layer7.sh
+chmod 0777 /root/start.sh
 
 
 
