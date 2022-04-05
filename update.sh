d7a0f3edc1707a55c6a35d60dc5e74f46a065c11
sania-owner <saniaowner@ya.ru>
2022-04-05 16:24:25 +0300

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo pkill -9 -f "python3"
+rm -rf "$HOME"/DDoS
+wget https://raw.githubusercontent.com/sania-owner/DDoS/main/install.sh; sudo chmod ugo+x install.sh; sh ./install.sh
+
