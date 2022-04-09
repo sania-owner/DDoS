abe26e64f2f5a538dcbef4379b4f2451227a139f
sania-owner <saniaowner@ya.ru>
2022-04-09 13:30:40 +0300

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 sudo pkill -9 -f "python3"; cd "$HOME"/DDoS/mhddos_proxy; python3 runner.py -t 5000 --itarmy --debug
-sleep 5
-cd "$HOME"/DDoS/mhddos_proxy; python3 runner.py -t 5000 --itarmy --debug
