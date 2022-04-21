fbc3e58716b319ba94298b5fd1c06b28cfc309c0
sania-owner <saniaowner@ya.ru>
2022-04-21 18:06:05 +0300

    Исправляет зависание python3 при обновлении целей

--- cron.sh
+++ cron.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+sudo pkill -9 -f "python3"; 
+sleep 5
+sudo pkill -9 -f "python3"; 
+sleep 5
 sudo pkill -9 -f "python3"; cd "$HOME"/DDoS/mhddos_proxy; python3 runner.py -t 5000 --itarmy
